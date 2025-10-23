ff5c8b06a0898cc14f5f3d9fa2bdb445b8358d96
NIRJALA DATTATRAY NAIK <158537275+nirjalanaik1706@users.noreply.github.com>
2025-10-23 20:33:20 +0530

    Update AsyncCoffeeShop.java

--- Multithreading/AsyncCoffeeShop.java
+++ Multithreading/AsyncCoffeeShop.java
@@ -4,7 +4,8 @@ import java.util.concurrent.CompletableFuture;
 
 public class AsyncCoffeeShop 
 {
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         System.out.println("Order placed for coffee ");
         
         CompletableFuture<Void> coffeeready = CompletableFuture.runAsync(()->
