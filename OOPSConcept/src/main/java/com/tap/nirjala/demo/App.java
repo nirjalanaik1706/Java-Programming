db02070ba011a5a6c753001d89f6fa6499d16d92
nirjalanaik1706 <nirjalanaik1706@gmail.com>
2026-01-25 10:26:11 +0530

    Add interface

--- OOPSConcept/src/main/java/com/tap/nirjala/demo/App.java
+++ OOPSConcept/src/main/java/com/tap/nirjala/demo/App.java
@@ -1,17 +1,30 @@
 package com.tap.nirjala.demo;
 
-import com.tap.nirjala.demo.HR.Employee;
+import com.tap.nirjala.demo.HR.Interfaces.IAppraisable;
+import com.tap.nirjala.demo.HR.Interfaces.IBonusEligible;
+import com.tap.nirjala.demo.HR.Interfaces.IInterviewPanel;
+import com.tap.nirjala.demo.HR.Interfaces.ITrainer;
 import com.tap.nirjala.demo.HR.SalesManager;
 
 public class App {
     public static void main(String[] args) {
 
-        Employee emp2 = new SalesManager(2,"Neha","Patil","neha@company.com","8888888888","Pune",1500, 7000, 30000, 200000, 250000, 10000);
-        emp2.doWork();
-        System.out.println(emp2);
-        System.out.println("Salary: " + emp2.computePay());
+        SalesManager manager = new SalesManager(2,"Neha","Patil","neha@company.com","8888888888","Pune",1500, 7000, 30000, 200000, 250000, 10000);
+        manager.doWork();
+        System.out.println(manager);
+        System.out.println("Salary: " + manager.computePay());
+        
+        IAppraisable appraisable=manager;
+        appraisable.ConductAppraisal();
 
+        IBonusEligible bonusEligible=manager;
+        bonusEligible.CalculateBonus();
         
-        
+        IInterviewPanel panel=manager;
+        panel.TakeInterview();
+
+        ITrainer trainer=manager;
+        trainer.Train();
     }
 }
+ 
