996d8dbbe7c7142e396a6cd4224c965a4b03f5cb
nirjalanaik1706 <nirjalanaik1706@gmail.com>
2026-01-31 22:39:52 +0530

    Formated

--- OOPSALLBASIC_CONCEPTS/src/main/java/com/tap/nirjala/demo/App.java
+++ OOPSALLBASIC_CONCEPTS/src/main/java/com/tap/nirjala/demo/App.java
@@ -8,7 +8,7 @@ import com.tap.nirjala.demo.shallow_deep_copy.Clone;
 
 public class App {
 
-    public static void main(String[] args)throws CloneNotSupportedException {
+    public static void main(String[] args) throws CloneNotSupportedException {
         //     //static class
         //     System.out.println( "Hello World!" );
         //     HRDepartment hd=new HRDepartment();
@@ -32,24 +32,23 @@ public class App {
         // hr2.employee.name="sahil";
         // System.out.println(hr2.employee.name);
         // System.out.println(hr1.employee.name); //shallow copy
-        
         // deep copy
-      
         // Employee_deep ed=new Employee_deep("nirjala");
         // HR_deep empdeep = new HR_deep( ed,"intern", 2);
-
         // Employee_deep ed2=new Employee_deep(ed);
         // ed2.name="sahil";
         // System.out.println("Original name :"+ ed);
         // System.out.println("copy name: "+ed2);
         // System.out.println(empdeep);
-
         // clone
-        Clone c1=new Clone("Nirjala",2);
+        Clone c1 = new Clone("Nirjala");
+        // System.out.println(c1);
+
+        Clone c2 = (Clone) c1.clone();
+        c2.Name = "sahil";
+        c1.Name = "sanika";
+        System.out.println(c2);
         System.out.println(c1);
 
-        Clone c2=(Clone) c1.clone();
-        System.out.println(c2);
-
     }
-}
+} 
--- OOPSALLBASIC_CONCEPTS/src/main/java/com/tap/nirjala/demo/shallow_deep_copy/Clone.java
+++ OOPSALLBASIC_CONCEPTS/src/main/java/com/tap/nirjala/demo/shallow_deep_copy/Clone.java
@@ -1,22 +1,19 @@
 package com.tap.nirjala.demo.shallow_deep_copy;
 
 public class Clone implements Cloneable {
-    String Name;
-    int id;
+    public String Name;
 
-    public Clone(String Name,int id){
+    public Clone(String Name){
         this.Name=Name;
-        this.id=id;
     }
 
     public void show(){
         System.out.println("Name is: "+Name);
-        System.out.println("ID :"+id);
     }
 
     @Override
 public String toString() {
-    return Name + " " + id;
+    return Name ;
 }
     public Object clone() throws CloneNotSupportedException{
         return super.clone();
